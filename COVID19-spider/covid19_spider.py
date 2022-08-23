1fc0784432c3f24a6d8ec15bf89acdcf9b91cf7f
Li Runze <lirunze.me@gmail.com>
2022-08-23 01:41:40 -0700

    Finish COVID19 spider

--- COVID19-spider/covid19_spider.py
+++ COVID19-spider/covid19_spider.py
@@ -3,27 +3,28 @@ FilePath: covid19_spider.py
 Author: LiRunze
 Date: 2022-08-22 22:59:38
 LastEditors: LiRunze
-LastEditTime: 2022-08-23 00:05:19
+LastEditTime: 2022-08-23 01:40:19
 Description:
 '''
 
 from cgitb import html
+from email import header
 import re
 import requests
 import time
 from bs4 import BeautifulSoup
 
-def getHTML(url):
+def getHTML(url, headers):
     try:
-        r = requests.get(url, timeout=30)
+        r = requests.get(url, headers=headers, timeout=30)
         r.raise_for_status()
         r.encoding = r.apparent_encoding
         return r.text
     except:
         return ""
 
-def getContent(url):
-    html = getHTML(url)
+def getContent(url, headers):
+    html = getHTML(url, headers)
     soup = BeautifulSoup(html, 'html.parser')
     paras_tmp = soup.select('.gk_title') + soup.select('p')
     paras = paras_tmp[0:]
@@ -41,7 +42,10 @@ def saveFile(text):
 
 def main():
     url = 'https://www.bengbu.gov.cn/public/22601/49996408.html'
-    text = getContent(url)
+    headers = {
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36"
+    }
+    text = getContent(url, headers)
     saveFile(text)
 
 main()

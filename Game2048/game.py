8c3efeb3850fcd17767d880b5bb4bf0749ee2f99
Li Runze <lirunze.me@gmail.com>
2022-08-23 20:43:57 -0700

    Update function'main' in 2048

--- Game2048/game.py
+++ Game2048/game.py
@@ -3,6 +3,30 @@ FilePath: game.py
 Author: LiRunze
 Date: 2022-08-23 20:31:46
 LastEditors: LiRunze
-LastEditTime: 2022-08-23 20:31:47
+LastEditTime: 2022-08-23 20:38:57
 Description: 
 '''
+
+import os
+import sys
+import numpy
+import random
+import pygame
+
+"""
+Form(): 窗口的设置
+Action(): 用户行为: 按键/鼠标
+InitGame(): 游戏初始化
+CreatNum(): 随机在一个位置生成一个数
+GetEmpty(): 获取空白方格
+MoveUp(): 向上移动
+MoveDown(): 向下移动
+MoveLeft(): 向左移动
+MoveRight(): 向右移动
+JudgeGameOver(): 判断游戏是否结束
+JudgeGameSuccess(): 判断游戏是否成功
+Paint(): 绘制表格
+"""
+
+class Game2048(object):
+
--- Game2048/main.py
+++ Game2048/main.py
@@ -3,6 +3,29 @@ FilePath: main.py
 Author: LiRunze
 Date: 2022-08-23 20:31:51
 LastEditors: LiRunze
-LastEditTime: 2022-08-23 20:31:51
+LastEditTime: 2022-08-23 20:42:46
 Description: 
 '''
+
+import configs
+from game import Game2048
+
+def main(args):
+    '''
+    screen_width: Width of the form
+    screen_height: Height of the form
+    block_gap: Gap between two blocks
+    block_size: Size of a block
+    '''
+    width   = args.screen_width
+    height  = args.screen_height
+    gap     = args.block_gap
+    size    = args.block_size
+    arc     = args.block_arc
+
+    g = Game2048(width, height, gap, size, arc)
+    g.Form()
+
+if __name__ == '__main__':
+    args = configs.parse_args()
+    main(args)
